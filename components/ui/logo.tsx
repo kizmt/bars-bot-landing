7bc2e33ed06dd6d3144c4cbbff1e061078fdce80
kizmt <kizmetcrypto@gmail.com>
2023-12-11 12:53:42 +1300

    initial commit

---
+++ components/ui/logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <image href="/images/logo.png" height="32" width="32"/>
+    </svg>
+  );
+}
